5a2b80f57c9ea7c0219e9c97afcd708ac57cc750
liuxiyuan-2022 <2695784289@qq.com>
2022-05-24 20:59:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import driver
+from works.work_1.correct import work_1_correct
 from works.work_2.correct import work_2_correct
 
 if __name__ == "__main__":
     driver = driver.diver_web()
-   # work_1_correct(driver)
-    work_2_correct(driver)
+    work_1_correct(driver)
+    #  work_2_correct(driver)
